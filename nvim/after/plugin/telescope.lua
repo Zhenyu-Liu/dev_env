931d326ee756d44d352545dccbad3fee4b947c28
liuzhenyu <jasonliu4706@gmail.com>
2024-08-28 15:23:30 -0700

    telescope setting

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -6,7 +6,9 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function() builtin.grep_string({ search = vim.fn.input("Grep > ") }); end)
 
-vim.keymap.set('n', '<leader>gi', builtin.lsp_definitions, {})
-vim.keymap.set('n', '<leader>gu', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
+vim.keymap.set('n', '<leader>gi', builtin.lsp_implementations, {})
+vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {})
+
 
 require('telescope').load_extension('fzf')
